e0dd91088a1b7ba3e36640f1ede449ca5e92f8ed
tkoar <terrancekoar@gmail.com>
2017-05-15 18:10:55 -0400

    Done.

--- app.rb
+++ app.rb
@@ -4,5 +4,20 @@ class App < Sinatra::Base
 		erb :index
 	end
 
+	get '/info' do
+		erb :info
+	end
+
+	get '/hello' do
+		erb :hello
+	end
+
+	get '/goodbye' do
+		erb :goodbye
+	end
+
+	get '/date' do
+		erb :date
+	end
 
 end
